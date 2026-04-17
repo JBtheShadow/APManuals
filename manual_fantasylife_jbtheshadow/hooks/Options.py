1f4ffcc1a410990c84aa050cb1f89d3168be99b2
JBtheShadow <jbtheshadow@gmail.com>
2026-04-17 16:41:13 -0300

    JB - editing game option groups

--- manual_fantasylife_jbtheshadow/hooks/Options.py
+++ manual_fantasylife_jbtheshadow/hooks/Options.py
@@ -98,8 +98,8 @@ def after_options_defined(options: Type[PerGameCommonOptions]):
 # Use this Hook if you want to add your Option to an Option group (existing or not)
 def before_option_groups_created(groups: dict[str, list[Type[Option[Any]]]]) -> dict[str, list[Type[Option[Any]]]]:
     # Uses the format groups['GroupName'] = [TotalCharactersToWinWith]
-    groups["Goal Options"] = [GoalRequirements]
-    groups["Life Options"] = [LivesAvailable]
+    groups["Game Options"].append(GoalRequirements)
+    groups["Life Options"].append(LivesAvailable)
     return groups
 
 def after_option_groups_created(groups: list[OptionGroup]) -> list[OptionGroup]:
